0036d9086df7897584ffc7bf3d6cfc8188fb9fd0
FranciG <beodo0@gmail.com>
2021-04-23 19:48:56 +0200

    part 1.4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
-/*1.3: course information step3
-Let's move forward to using objects in our application.
- Modify the variable definitions of the App component as follows and 
- also refactor the application so that it still works:*/ 
+/*1.4: course information step4
+And then place the objects into an array. Modify the variable definitions of App into the following form and modify the other parts of the application accordingly:*/ 
 const Header = (props) => {
   return (
     <div>
@@ -11,28 +9,37 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+/* const Content = (props) => {
   console.log(props)
   return (
     <div>
       <p>
-      {props.part1}  {props.exercises1}
-      </p>
-      <p>
-      {props.part2}  {props.exercises2}
-      </p>
-      <p>
-      {props.part3}  {props.exercises3}
+      {props.name}  
       </p>
     </div>
   )
-}
-//The "Total" Component is now looking for a prop that is called "totalExercises"
-const Total= ({ totalExercises }) => (
-  <p>Number of exercises: {totalExercises}</p> 
-  );
- 
+} */
 
+
+
+const Content = (props) => {
+  //console.log(props.parts);
+  return props.parts.map(item => (
+    <div>{item.name} {item.exercises}</div>
+    
+  ))
+};
+
+
+ const Total = (props) => {
+    console.log(props);
+    return <div>
+      <p>
+        
+      Number of exercises: {props.parts[0].exercises}+{props.parts[1].exercises}+{props.parts[2].exercises}
+      </p>
+      </div>;
+  };
 
 
 
@@ -41,28 +48,32 @@ const App = () => {
 
   
   const course = 'Half Stack application development'
- //objects
-  const part1 = {
+ //array
+ const parts = [
+  {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
-  
+]
+
+/* parts.forEach(value => {
+  console.log(value)   */
+/* })  */
+  //pass the parts array to the Content component. In order to receive the props
   return (
     <div>
         
       <Header  course={course} />
-      <Content  part1={part1.name} exercises1={part1.exercises} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part2.exercises}/>
-      <Total  totalExercises={part1.exercises+part2.exercises+part3.exercises}/>
-
+      <Content parts={parts} />
+      <Total parts={parts} />
       
   
       
