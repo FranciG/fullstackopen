944a49e2a448b68b2f2c92bf11e29c50cb0ba46c
FranciG <beodo0@gmail.com>
2021-04-24 13:35:24 +0200

    total sum fixed

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const Content = (props) => {
     return <div>
       <p>
         
-      Number of exercises: {props.parts[0].exercises}+{props.parts[1].exercises}+{props.parts[2].exercises}
+      Number of exercises: {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}
       </p>
       </div>;
   };
