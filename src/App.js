74d77163ac2b121c6c42609ca89d62a1fe618f02
FranciG <beodo0@gmail.com>
2021-04-24 14:27:18 +0200

    exercise 1.5

--- src/App.js
+++ src/App.js
@@ -2,37 +2,38 @@ import React from 'react'
 /*1.4: course information step4
 And then place the objects into an array. Modify the variable definitions of App into the following form and modify the other parts of the application accordingly:*/ 
 const Header = (props) => {
+  console.log(props)
+  //I pass the whole object (course) as props and use only name
   return (
     <div>
-      <h1>{props.course}</h1>
+    {   <h1>{props.course.name}</h1> }
     </div>
   )
 }
 
-/* const Content = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-      {props.name}  
-      </p>
-    </div>
-  )
-} */
+
 
 
 
 const Content = (props) => {
-  //console.log(props.parts);
-  return props.parts.map(item => (
-    <div>{item.name} {item.exercises}</div>
-    
-  ))
+  console.log(props);
+   //I pass as props only the array parts from the object course (course.parts)
+  return <div>
+    <p>
+      {props.content[0].name} {props.content[0].exercises}
+    </p>
+    <p>
+      {props.content[1].name} {props.content[1].exercises}
+    </p>
+    <p>
+      {props.content[2].name} {props.content[2].exercises}
+    </p>
+    </div>;
 };
-
 //Getting the data from the array without looping through it.
  const Total = (props) => {
     console.log(props);
+     //I pass as props only the array parts from the object course (course.parts)
     return <div>
       <p>
         
@@ -44,36 +45,31 @@ const Content = (props) => {
 
 
 
-const App = () => {
-
-  
-  const course = 'Half Stack application development'
- //array
- const parts = [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }
-]
-
-/* parts.forEach(value => {
-  console.log(value)   */
-/* })  */
-  //pass the parts array to the Content component. In order to receive the props
+  const App = () => {
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    }
+    
   return (
     <div>
         
       <Header  course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content content={course.parts}/>
+      <Total parts={course.parts}/>
       
   
       
