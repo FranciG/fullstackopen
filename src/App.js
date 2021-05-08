180a74b43b082fa7c3a4a6471292a211f4708ff3
FranciG <beodo0@gmail.com>
2021-05-08 15:53:32 +0200

    1.13 code explained

--- src/App.js
+++ src/App.js
@@ -16,13 +16,27 @@ const App = () => {
  //Get the leght of the array to generate numbers as long as the array is.
   const dataLenght = anecdotes.length;
   const randomNo = () => Math.floor(Math.random() * dataLenght);
-//First parameter:initial value of state. Second parameter: function that will be used to modify the state.
-//The initial value depends of the value that randomNo has. Thus, the position of the array anecdotes displayed changes randomly. 
+/* UseState: Funtion call to the imported above useState funtion
+the component and renders it initialized with the value of randomNo.
+The function returns an array that contains two items. 
+We assign the items to the variables counter and setCounter by using the destructuring assignment syntax shown earlier.
+
+The select variable is assigned the initial value of state which is  the value of randomNo. 
+The variable setSelected is assigned to a function that will be used to modify the state.
+
+When the state modifying function setSelect is called by clicking the button,
+React re-renders the component which means that the function body of the component function gets re-executed
+*/
   const [selected, setSelected] = useState(randomNo)
 
   //In this case, the initial state value is an array full of zeroes.
   const [votes, setVotes] = useState(Array(dataLenght).fill(0));
-  
+  /* When clicking the button vote, the function vote below is executed,
+  passing the current anecdote as a parameter.
+  Then a copy of votes array (that is initially only zeroes) is created.
+  The position that belongs to the current anecdote is increased by one on each click
+  and the new array is set as state, causing the component to re-render.
+   */
   const vote = selected => {
     const copy = [...votes];
     copy[selected] += 1;
