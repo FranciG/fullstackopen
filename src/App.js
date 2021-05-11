1e3c5ddfb117742a1ac1fd0037fe151fb5c736a3
FranciG <beodo0@gmail.com>
2021-05-11 13:22:34 +0200

    2.1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-/*1.4: course information step4
-And then place the objects into an array. Modify the variable definitions of App into the following form and modify the other parts of the application accordingly:*/ 
+
+
 const Header = (props) => {
-  console.log(props)
+  //console.log(props)
   //I pass the whole object (course) as props and use only name
   return (
     <div>
@@ -12,64 +12,53 @@ const Header = (props) => {
 }
 
 
-
-
-
-const Content = (props) => {
-  console.log(props);
-   //I pass as props only the array parts from the object course (course.parts)
-  return <div>
-    <p>
-      {props.content[0].name} {props.content[0].exercises}
-    </p>
-    <p>
-      {props.content[1].name} {props.content[1].exercises}
-    </p>
-    <p>
-      {props.content[2].name} {props.content[2].exercises}
-    </p>
-    </div>;
-};
-//Getting the data from the array without looping through it.
- const Total = (props) => {
-    console.log(props);
-     //I pass as props only the array parts from the object course (course.parts)
-    return <div>
-      <p>
-        
-      Number of exercises: {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}
-      </p>
-      </div>;
-  };
-
-
-
-
   const App = () => {
+    
     const course = {
+      id: 1,
       name: 'Half Stack application development',
+    
       parts: [
         {
           name: 'Fundamentals of React',
-          exercises: 10
+          exercises: 10,
+          id: 1
         },
         {
           name: 'Using props to pass data',
-          exercises: 7
+          exercises: 7,
+          id: 2
         },
         {
           name: 'State of a component',
-          exercises: 14
+          exercises: 14,
+          id: 3
         }
       ]
+
     }
+    
+    //const Result = course.parts.map(parts => <li key={parts.id}>{parts.name}</li>)
+    //console.log(Result)
+
+
+
+
+  
     
   return (
     <div>
         
-      <Header  course={course} />
-      <Content content={course.parts}/>
-      <Total parts={course.parts}/>
+        <Header  course={course} />
+       
+      <ul>
+      {course.parts.map(part => 
+          <li key={part.id}>
+            {part.name} {part.exercises}
+          </li>
+        )}
+      </ul>
+    
       
   
       
