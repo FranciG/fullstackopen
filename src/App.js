2761892a40c43591129c295e10c4ce9c278ccab9
FranciG <beodo0@gmail.com>
2021-05-17 14:04:08 +0200

    2.6 Phonebook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    //The array persons is empty at start
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  //adding new persons
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    /* complete the addPerson function for creating new persons */
+    const personObject = {
+      name: newName,
+      //date: new Date().toISOString(),
+      //important: Math.random() < 0.5,
+      id: persons.length + 1,
+    }
+  //personObject data is contatenated to the persons array
+  //State is updated
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  
+  
+  
+  }
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+     
+      <form onSubmit={addPerson}>
+        <div>
+          name: 
+          <input 
+          value={newName}
+          onChange={handlePersonChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+
+      {console.log(persons)}
+      <ul>
+      {persons.map(person => 
+          <li key={person.id}>
+            {person.name} 
+          </li>
+        )}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
