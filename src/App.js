434af6749a144f7d45c245980ec1db53544fcc87
FranciG <beodo0@gmail.com>
2021-05-30 18:13:53 +0200

    2.12

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+//setCountries is a function for setting the country's state
+const App = () => {
+  const [countries, setCountries] = useState([])
+//Filter
+const [searchFilter, setSearchFilter] = useState('')
+  
+const hook = () => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+        
+      })
+  }
 
-function App() {
+  useEffect(hook,[])
+/*   by default the effect is always run after the component has been rendered. In our case, however, we only want to execute the effect along with the first render.
+
+  The second parameter of useEffect is used to specify how often the effect is run. If the second parameter is an empty array [], then the effect is only run along with the first render of the component. */
+
+  console.log('render', countries.length, 'countries')
+  console.log(countries)
+
+
+
+
+/* //To display the countries array data without typing
+const showCountries = () => {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <ul>
+      {countries.map((country) => 
+          <p key={country.alpha2Code}>{country.name}</p>
+        )}
+      </ul>
     </div>
+  )
+} */
+
+const filteredCountries =
+searchFilter.length === 1
+? countries
+: countries.filter(
+(country) => country.name.toLowerCase().indexOf(searchFilter.toLowerCase()) > -1
+)
+
+//showCountries returns either a message or else the contents of filteredcountries array
+const showCountries = () => {
+
+if (filteredCountries.length === 0) {
+    return 'No coincidences found'  
+}
+if (filteredCountries.length > 10) {
+return 'Too many matches'
+}
+
+
+  if (filteredCountries.length > 10) {
+    return filteredCountries.map((country) =>
+<p key={country.alpha2Code}>{country.name}</p>
+)
+    }
+    if (filteredCountries.length === 1) {
+      return filteredCountries.map((country) =>
+  <p key={country.alpha2Code}>Capital: {country.capital}. Population: {country.population} <img src={country.flag}/></p>
+
+  )
+      }    
+
+}
+const searchHandler = (e) => {
+setSearchFilter(e.target.value)
+}
+
+  return (
+<div>
+
+<div>
+<h1>Countries</h1>
+</div>
+<div>
+Type to find countries: 
+<input onChange={searchHandler} />
+<div>
+{showCountries()}
+</div>
+</div>
+
+</div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+/* import axios from 'axios' */
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+/* Storing the promise object in a variable is generally unnecessary, and it's instead common to chain the then method call to the axios method call
+const promise = axios.get('https://restcountries.eu/rest/v2/all') 
+//console.log(promise)
+/* to access the result of the operation represented by the promise, we must register an event handler 
+to the promise. This is achieved using the method then: 
+promise.then(response => {
+  console.log(response)
+})
+*/
+
+
+/* axios
+  .get('https://restcountries.eu/rest/v2/all')
+  .then(response => {
+    const countries = response.data
+    console.log(countries)
+  }) */
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App 
+    
+    
+    
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
